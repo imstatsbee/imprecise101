f20de7be319627a791a9aebc46ec1edf8025c4aa
imstatsbee <imstatsbee@gmail.com>
2023-01-24 23:26:42 -0700

    Update walley1996.R

--- R/walley1996.R
+++ R/walley1996.R
@@ -3,8 +3,17 @@
 #' @title Means and Standard Deviations of $\theta_R$
 #' @description Formulae in Section 3
 #' @export
-iddm.inf <- function(nj, s=1, N){
+iddm.inf <- function(nj, s=1, N, tj=NA){
   stopifnot(s >= 1)
+
+  # point probability p.10, 0 <= tj <= 1
+  p <- (nj+s*tj)/(N+s)
+  # upper bound of expectation (probability) p.10
+  p.u <- (nj+s)/(N+s) # limit as tj -> 1
+  # lower bound of expectation (probability) p.10
+  p.l <- (nj)/(N+s) # limit as tj -> 0
+  # p.delta <- p.u - p.l
+  p.delta <- s/(N+s)
 
   # uppder bound of Variance (p.17)
   n.u <- nj*(N-nj) + 1/4*(N+s)*(s+1)^2
@@ -16,9 +25,23 @@ iddm.inf <- function(nj, s=1, N){
   d.l <- (N+s)^2*(N+s+1)
   v.l <- n.l/d.l
 
-  robj <- list(v.lower=v.l, v.upper=v.u)
+  robj <- list(p.lower=p.l, p.upper=p.u, v.lower=v.l, v.upper=v.u, s.lower=sqrt(v.l), s.upper=sqrt(v.u), p=p, p.delta=p.delta)
   return(robj)
 }
 
 # 4.2. Means and standard deviations of theta_R
-lapply(iddm.inf(nj=1, N=6, s=2), sqrt)
+iddm.inf(nj=1, N=6, s=2) # pass
+iddm.inf(nj=1, N=6, s=1) # pass
+
+f <- function(theta){
+  v <- 105*theta^2*(1-theta)^4
+  return(v)
+}
+
+a1 <- a*(1-a)^5
+b1 <- b*(1-b)^5
+a1 == b1
+
+library(pscl)
+betaHPD(alpha=2, beta=6, p=0.95, plot=TRUE)
+
